a5cd89204528552cf86495c6361499c7cc4d3389
MasterUNG <phrombutr@gmail.com>
2018-03-09 14:24:51 +0700

    Update Multiple Data to Firebase

---
+++ app/src/main/java/masterung/androidthai/in/th/qrfirebase/fragment/ReadAllCodeFragment.java
@@ -0,0 +1,24 @@
+package masterung.androidthai.in.th.qrfirebase.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import masterung.androidthai.in.th.qrfirebase.R;
+
+/**
+ * Created by masterung on 9/3/2018 AD.
+ */
+
+public class ReadAllCodeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_read_all, container, false);
+        return view;
+    }
+}
